d0ea73db5413302d7561ae074b41869bff6ae434
Namrata <ndubeyansu@gmail.com>
2026-04-22 19:59:17 +0530

    UPDATED the port

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,10 @@ app.use(express.json());
 connectDB();
 
 // starting server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 app.get('/',(req,res)=>{
   res.send("welcome to storybase backend!")
